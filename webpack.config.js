b4d23d2d2a29c60de060fc88c76321c718c80aef
victorbofill <victorm.bofill@gmail.com>
2018-05-29 17:30:28 -0700

    initial webpack.config functional; index.js working

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
