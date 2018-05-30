d347d575f49e53e5ebfcabc90df9ef3119414888
victorbofill <victorm.bofill@gmail.com>
2018-05-30 09:52:44 -0700

    image upload and image display functioning

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,52 @@ export default class App extends Component {
     super();
 
     this.state = {
-      
+      image: 'https://www.pixelstalk.net/wp-content/uploads/2016/10/Blank-Wallpaper-HD-620x388.jpg',
+    };
+  }
+
+  handleImageSrc({ target }) {
+    this.setState({ image: target.value });
+  }
+
+  handleUpload({ target }) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(target.files[0]);
+
+    reader.onload = () => {
+      this.setState({ image: reader.result });
     };
   }
 
   render() {
+    const { image } = this.state;
+
     return (
       <main>
-        <h1>Test</h1>
+        <section>
+          <div>
+            <label>
+              Upload Image: 
+              <input onChange={event => this.handleImageSrc(event)}/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              <input
+                type="file"
+                onChange={event => this.handleUpload(event)}
+              />
+            </label>
+          </div>
+
+          <div className="image-container"
+            ref={node => this.imageExport = node}
+          >
+            <img src={image}/>
+          </div>
+        </section>
       </main>
     );
   }
